e9521b276563a6fef7568448674da016077a7b7c
alexxchela <alex960513@gmail.com>
2020-11-14 23:26:26 -0600

    Second commit. CoverUncover animation

---
+++ EyesMovement/Assets/Project/Scripts/CameraSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    GameObject MainCamera;
+    //GameObject LeftEyeCam;
+    //GameObject RightEyeCam;
+    GameObject BothEyes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("MainCamera");
+        //LeftEyeCam = GameObject.Find("LeftEyeCam");
+        //RightEyeCam = GameObject.Find("RighEyeCam");
+        BothEyes = GameObject.Find("InfrontCam");
+        MainCamera.SetActive(true);
+        BothEyes.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("SwitchCam"))
+        {
+            if(MainCamera.activeSelf)
+            {
+                MainCamera.SetActive(false);
+                BothEyes.SetActive(true);
+            }
+            else
+            {
+                MainCamera.SetActive(true);
+                BothEyes.SetActive(false);
+            }
+        }
+    }
+}
---
+++ EyesMovement/Assets/Project/Scripts/CoverUncover.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CoverUncover : MonoBehaviour
+{
+    GameObject occluder;
+    GameObject leftEye;
+    GameObject rightEye;
+
+
+     Vector3 leftFront;
+    Vector3 leftBellow;
+    Vector3 rightFront;
+    Vector3 rightBellow;
+
+    Vector3[] path;
+    [SerializeField] float duration;
+    [SerializeField] PathMode pathMode;
+    [SerializeField] PathType pathType;
+        
+    bool play =false;
+    void Start()
+    {
+        occluder = GameObject.Find("Occluder");
+        leftEye = GameObject.Find("LEye");
+        rightEye = GameObject.Find("REye");
+        SetParades();
+    }
+
+
+    void SetParades()
+    {
+        leftFront = new Vector3(-1.3099f, 0.5f, 1);
+        leftBellow = new Vector3 (-1.3099f, -1, 1);
+        rightFront = new Vector3(1.3099f, 0.5f, 1);
+        rightBellow = new Vector3 (1.3099f, -1, 1);
+
+        path = new Vector3 [12];
+        path[0] = leftFront;
+        path[1] = leftBellow;
+        path[2] = leftFront;
+        path[3] = leftBellow;
+        path[4] = leftFront;
+        path[5] = rightBellow;
+        path[6] = rightFront;
+        path[7] = rightBellow;
+        path[8] = rightFront;
+        path[9] = rightBellow;
+        path[10] = rightFront;
+        path[11] = rightBellow;
+    }
+
+
+    public void PlayAnimation()
+    {
+        Transform occluderTransform = occluder.transform;
+        occluderTransform.DOPath(path,duration,pathType,pathMode,10);
+    }
+    
+    
+}
